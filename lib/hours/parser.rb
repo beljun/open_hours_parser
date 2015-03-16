be3c380b23c7097cf0a3328b54aaf82a26b0249e
Junjun Olympia <romeo.olympia@gmail.com>
2015-03-17 02:55:46 +0800

    Chinese tests passing.

--- lib/hours/parser.rb
+++ lib/hours/parser.rb
@@ -20,12 +20,32 @@ module Hours
     def pre_normalize(text)
       t = text.dup.to_s.downcase
 
+      # Replace newlines
+      t = t.gsub "\n", ' '
+
+      # Chinese 
+      t = t.gsub '星期一', ' monday -'
+      t = t.gsub '至五', ' friday '
+
+      t = t.gsub '至六', ' saturday '
+      t = t.gsub '星期六、日及公眾假期', ' saturday - sunday ' 
+      t = t.gsub '至日', ' sunday '
+      t = t.gsub '星期日', ' sunday '
+      t = t.gsub '日及公眾假期', ' sunday '
+      t = t.gsub '星期日及公眾假期', ' sunday '
+
+      # Common expressions
+      t = t.gsub 'weekday', ' monday - friday '
+
       # strange unicode
       t = t.gsub '：', ': '
       t = t.gsub '；', '; '
       t = t.gsub '–', '-'
-      
-      t = t.gsub /([-;,.&])/, ' \1 ' 
+      t = t.gsub '一', '-'
+
+      t = t.gsub /([-;,.&])/, ' \1 '
+      t = t.gsub '(', ' ( '
+      t = t.gsub ')', ' ) '
     end
   end
 end
